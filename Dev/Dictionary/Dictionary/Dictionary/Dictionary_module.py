6647c8621fd39727dd26a712cd9241acb2eca28b
ShwetabhK <shwetabh002@gmail.com>
2018-01-28 00:52:46 +0530

    Django-Dictionary-commit1

---
+++ Dev/Dictionary/Dictionary/Dictionary/Dictionary_module.py
@@ -0,0 +1,27 @@
+import json
+import difflib
+from difflib import SequenceMatcher
+data=json.load(open("data.json",'r'))
+def definition(key):
+    value=""
+    for i in range(0,len(data[key])):      
+        value=value+data[key][i]+"\n"
+    return value
+def find(key):
+    ch=1
+    key=key.lower()
+   # try:
+    value=definition(key)
+    return value
+    #except:
+        #flag=0
+        #for i in data:
+         #   amt=SequenceMatcher(None,key,i).ratio()
+          #  if(amt>0.7):
+           #     print("Did you mean "+i+" ? Input yes or no")
+            #    flag=1
+             #   answer=input().lower()
+              #  if answer=="yes":
+               #     return definition(i)
+        #if(flag==0):
+         #   return "Word not found"
